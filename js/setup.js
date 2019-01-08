4ca1f3d042e07f808f7f02aa2dceaa6a614050d2
muggy8 <muggy8@gmail.com>
2019-01-08 03:00:32 -0800

    setup the app wrapper and a way to just define new custom elements on the fly for clearner looking markup

---
+++ js/setup.js
@@ -0,0 +1,21 @@
+const app = {}
+
+// new block to keep the custom element function properties scoped to the special function so nothing else can really mess with it too much except by directly getting it via dom but it's so generic that's gonna be kinda hard.
+{
+	let customElementStyler = document.head.appendChild(document.createElement("style"))
+	let elementList = []
+
+	let renderCSS = function(){
+		customElementStyler.innerHTML = elementList.join(",") + "{display: block}"
+	}
+
+	app.customElement = function(domString, constructorClass = class extends HTMLElement {}){
+		customElements.define(domString, constructorClass)
+
+		elementList.push(domString)
+		renderCSS()
+	}
+}
+
+
+app.customElement("app-main")
