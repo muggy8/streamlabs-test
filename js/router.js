47433cadaddaa7f8fd03874a8b361318074e5ff4
muggy8 <muggy8@gmail.com>
2019-01-08 15:57:43 -0800

    add login frame

--- js/router.js
+++ js/router.js
@@ -1,6 +1,6 @@
 gapi.load('client:auth2', async function(){
 
-	// the configs are imported as a poromise in the setup.js
+	// the configs are imported as a promise in the setup.js
 	let configs = await app.configsAsync
 
 	await gapi.client.init({
