1e83d01607aacc4dc38f5126390a26c6c5fa4ce3
muggy8 <muggy8@gmail.com>
2019-01-10 01:25:45 -0800

    is able to successfully fetch the chat messages from the youtube API

--- js/components/stream-watch.js
+++ js/components/stream-watch.js
@@ -19,15 +19,68 @@ app.controllers.streamWatch.ajax = fetch("js/components/stream-watch.html")
 			messages: []
 		}
 
-		async function fetchChat(){
-			if (!controller.chat.open){
+		function wait(ms){
+			return new Promise(function(accept){
+				setTimeout(accept, ms)
+			})
+		}
+
+		async function fetchChatRecursive(){
+			console.log("chat open toggled")
+			if (!controller.streamDeets.activeLiveChatId){
 				return
 			}
 
-			await gapi.client.youtube.liveChatMessages.list({
+			let currentChat = await gapi.client.youtube.liveChatMessages.list({
 				part: "snippet",
-				liveChatId: controller.videoId
+				liveChatId: controller.streamDeets.activeLiveChatId,
+				pageToken: controller.chat.nextPageToken || undefined
 			})
+
+			console.log(currentChat)
+
+			currentChat.result.items.forEach(message=>controller.chat.messages.push(message))
+
+			controller.chat.nextPageToken = currentChat.result.nextPageToken
+
+			await wait(currentChat.result.pollingIntervalMillis)
+			fetchChatRecursive()
+		}
+
+		controller.watch("chat.open", fetchChatRecursive)
+
+		controller.watch("videoId", async function(){
+			controller.chat.open = false;
+
+			if (controller.chat.messages.length){
+				controller.chat.messages.splice(0, controller.chat.messages.length)
+			}
+
+			controller.streamDeets = {}
+
+			if (!controller.videoId){
+				return
+			}
+
+			let liveDeets = await gapi.client.youtube.videos.list({
+				part: "liveStreamingDetails",
+				id: controller.videoId
+			})
+			controller.streamDeets = liveDeets.result.items[0].liveStreamingDetails
+		})
+
+		controller.onDetach = function(){
+			controller.chat.open = false;
+
+			if (controller.chat.messages.length){
+				controller.chat.messages.splice(0, controller.chat.messages.length)
+			}
+
+			controller.streamDeets = {}
+
+			controller.videoId = null
+
+			controller.chat.nextPageToken = null
 		}
 
 		controller.view = proxymity(template, app.controllers.streamWatch)
