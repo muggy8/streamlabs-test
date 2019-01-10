61792149091501c1916f05b1853b3cde73b3caa4
muggy8 <muggy8@gmail.com>
2019-01-09 23:55:11 -0800

    made a chat handle that slides out

--- js/components/stream-watch.js
+++ js/components/stream-watch.js
@@ -8,13 +8,28 @@ app.controllers.streamWatch.ajax = fetch("js/components/stream-watch.html")
 
 		controller.routRegex = /^\/stream\/([^\/]+)/
 
-		controller.view = proxymity(template, app.controllers.streamWatch)
-
 		controller.onAttach = function(){
 			document.location.pathname.replace(controller.routRegex, function(whole, videoId){
 				controller.videoId = videoId
 			})
 		}
 
+		controller.chat = {
+			open: false,
+			messages: []
+		}
+
+		async function fetchChat(){
+			if (!controller.chat.open){
+				return
+			}
+
+			await gapi.client.youtube.liveChatMessages.list({
+				part: "snippet",
+				liveChatId: controller.videoId
+			})
+		}
+
+		controller.view = proxymity(template, app.controllers.streamWatch)
 	})
 	.catch(app.catchPromiseError)
